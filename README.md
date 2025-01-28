ce258afe8fce25fe9ca8336521d39c46653900b9
Gobrin <gobrin3707@saskpolytech.ca>
2025-01-28 12:22:48 -0600

    Push original code

---
+++ README.md
@@ -0,0 +1,1 @@
+The backend layer for both web and app interfaces for the Archie Bell project.
---
+++ playground/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+# REQUEST -> RESPONSE
+
+def say_hello(request):
+    return HttpResponse('Hello, world.')
