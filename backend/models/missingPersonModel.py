d4f37a5e986fa9842598b388455e1e258d164fcd
AishaAbioye <101652630+AishaAbioye@users.noreply.github.com>
2025-01-28 11:11:40 -0600

    Implement backend functionality (non-functional)

---
+++ backend/models/missingPersonModel.py
@@ -0,0 +1,15 @@
+  #  Added by Aishat
+from django.db import models
+
+class MissingPerson(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.PositiveIntegerField()
+    last_location_seen = models.CharField(max_length=255)
+    last_date_time_seen = models.DateTimeField()
+    additional_info = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='images/')
+
+    class Meta:
+        db_table = "missing_person_list"
+        verbose_name = "Missing Person"
+        verbose_name_plural = "Missing Persons"
