5572657cdf0de2921bf16c54f87b74f067e73216
Gobrin <gobrin3707@saskpolytech.ca>
2025-04-02 04:50:04 -0600

    Partially implement dual websocket handling

--- database/consumers.py
+++ database/consumers.py
@@ -2,7 +2,7 @@ import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 
-class NotificationConsumer(WebsocketConsumer):
+class SubmissionConsumer(WebsocketConsumer):
     def connect(self):
         self.room_group_name = 'updates'
         async_to_sync(self.channel_layer.group_add)(
@@ -45,10 +45,38 @@ class NotificationConsumer(WebsocketConsumer):
             'message': message
         }))
         
-    def found_submission(self, event):
+class ActiveSearchConsumer(WebsocketConsumer):
+    def connect(self):
+        self.room_group_name = 'active_search'
+        
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+        
+        self.accept()
+        
+        self.send(text_data=json.dumps({
+            'type': 'connection_established',
+            'message': 'WebSocket connected'
+        }))
+        
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json.get('message', None)
+        
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'active_search_message',
+                'message': message
+            }
+        )
+        
+    def active_search_message(self, event):
         message = event['message']
         
         self.send(text_data=json.dumps({
-            'type': 'found_update',
+            'type': 'active_search_update',
             'message': message
         }))
--- database/routing.py
+++ database/routing.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/submission-updates/', consumers.NotificationConsumer.as_asgi())
+    re_path(r'ws/submission-updates/', consumers.SubmissionConsumer.as_asgi()),
+    re_path(r'ws/active-search-updates/', consumers.ActiveSearchConsumer.as_asgi())
 ]
